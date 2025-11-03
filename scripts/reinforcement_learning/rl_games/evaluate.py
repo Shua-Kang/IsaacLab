1feffba0e47384c4d4ea405cff38847a529ec330
shua kang <jiuerrose@outlook.com>
2025-11-03 14:25:48 -0500

    init

---
+++ scripts/reinforcement_learning/rl_games/evaluate.py
@@ -0,0 +1,250 @@
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers (https://github.com/isaac-sim/IsaacLab/blob/main/CONTRIBUTORS.md).
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""Script to play a checkpoint if an RL agent from RL-Games."""
+
+"""Launch Isaac Sim Simulator first."""
+
+import argparse
+import sys
+import time
+from isaaclab.app import AppLauncher
+
+# add argparse arguments
+parser = argparse.ArgumentParser(description="Play a checkpoint of an RL agent from RL-Games.")
+parser.add_argument("--video", action="store_true", default=False, help="Record videos during training.")
+parser.add_argument("--video_length", type=int, default=100, help="Length of the recorded video (in steps).")
+parser.add_argument(
+    "--disable_fabric", action="store_true", default=False, help="Disable fabric and use USD I/O operations."
+)
+parser.add_argument("--num_envs", type=int, default=None, help="Number of environments to simulate.")
+parser.add_argument("--task", type=str, default=None, help="Name of the task.")
+parser.add_argument(
+    "--agent", type=str, default="rl_games_cfg_entry_point", help="Name of the RL agent configuration entry point."
+)
+parser.add_argument("--seed", type=int, default=None, help="Seed used for the environment")
+
+parser.add_argument("--real-time", action="store_true", default=False, help="Run in real-time, if possible.")
+
+parser.add_argument("--mass_low", type=float, default=0.05, help="Lower bound for mass initialization.")
+parser.add_argument("--mass_high", type=float, default=0.05, help="Upper bound for mass initialization.")
+
+# append AppLauncher cli args
+AppLauncher.add_app_launcher_args(parser)
+# parse the arguments
+args_cli, hydra_args = parser.parse_known_args()
+# always enable cameras to record video
+if args_cli.video:
+    args_cli.enable_cameras = True
+
+# clear out sys.argv for Hydra
+sys.argv = [sys.argv[0]] + hydra_args
+# launch omniverse app
+app_launcher = AppLauncher(args_cli)
+simulation_app = app_launcher.app
+
+"""Rest everything follows."""
+
+
+import gymnasium as gym
+import math
+import os
+import random
+import time
+import torch
+
+from rl_games.common import env_configurations, vecenv
+from rl_games.common.player import BasePlayer
+from rl_games.torch_runner import Runner
+
+from isaaclab.envs import (
+    DirectMARLEnv,
+    DirectMARLEnvCfg,
+    DirectRLEnvCfg,
+    ManagerBasedRLEnvCfg,
+    multi_agent_to_single_agent,
+)
+from isaaclab.utils.assets import retrieve_file_path
+from isaaclab.utils.dict import print_dict
+from isaaclab.utils.pretrained_checkpoint import get_published_pretrained_checkpoint
+
+from isaaclab_rl.rl_games import RlGamesGpuEnv, RlGamesVecEnvWrapper
+
+import isaaclab_tasks  # noqa: F401
+from isaaclab_tasks.utils import get_checkpoint_path
+from isaaclab_tasks.utils.hydra import hydra_task_config
+import numpy as np
+# PLACEHOLDER: Extension template (do not remove this comment)
+
+
+@hydra_task_config(args_cli.task, args_cli.agent)
+def main(env_cfg: ManagerBasedRLEnvCfg | DirectRLEnvCfg | DirectMARLEnvCfg, agent_cfg: dict):
+    """Play with RL-Games agent."""
+    # grab task name for checkpoint path
+    task_name = args_cli.task.split(":")[-1]
+    train_task_name = task_name.replace("-Play", "")
+
+    # override configurations with non-hydra CLI arguments
+    env_cfg.scene.num_envs = args_cli.num_envs if args_cli.num_envs is not None else env_cfg.scene.num_envs
+    env_cfg.sim.device = args_cli.device if args_cli.device is not None else env_cfg.sim.device
+
+    # randomly sample a seed if seed = -1
+    if args_cli.seed == -1:
+        args_cli.seed = random.randint(0, 10000)
+
+    agent_cfg["params"]["seed"] = args_cli.seed if args_cli.seed is not None else agent_cfg["params"]["seed"]
+    # set the environment seed (after multi-gpu config for updated rank from agent seed)
+    # note: certain randomizations occur in the environment initialization so we set the seed here
+    env_cfg.seed = agent_cfg["params"]["seed"]
+
+    # specify directory for logging experiments
+    log_root_path = os.path.join("logs", "rl_games", agent_cfg["params"]["config"]["name"])
+    log_root_path = os.path.abspath(log_root_path)
+    print(f"[INFO] Loading experiment from directory: {log_root_path}")
+    # find checkpoint
+
+    # wrap around environment for rl-games
+    rl_device = agent_cfg["params"]["config"]["device"]
+    clip_obs = agent_cfg["params"]["env"].get("clip_observations", math.inf)
+    clip_actions = agent_cfg["params"]["env"].get("clip_actions", math.inf)
+    obs_groups = agent_cfg["params"]["env"].get("obs_groups")
+    concate_obs_groups = agent_cfg["params"]["env"].get("concate_obs_groups", True)
+    
+    env_cfg.enable_tactile = False
+    env_cfg.enable_tactile_camera = True
+    env_cfg.enable_gripper_camera = True
+    env_cfg.enable_global_camera = True
+    env_cfg.mass_range = [args_cli.mass_low, args_cli.mass_high]
+
+    # create isaac environment
+    env = gym.make(args_cli.task, cfg=env_cfg, render_mode="rgb_array" if args_cli.video else None)
+
+    # convert to single-agent instance if required by the RL algorithm
+    if isinstance(env.unwrapped, DirectMARLEnv):
+        env = multi_agent_to_single_agent(env)
+
+    # wrap for video recording
+    if args_cli.video:
+        video_kwargs = {
+            "video_folder": os.path.join(log_root_path, log_dir, "videos", "play"),
+            "step_trigger": lambda step: step == 0,
+            "video_length": args_cli.video_length,
+            "disable_logger": True,
+        }
+        print("[INFO] Recording videos during training.")
+        print_dict(video_kwargs, nesting=4)
+        env = gym.wrappers.RecordVideo(env, **video_kwargs)
+
+    # wrap around environment for rl-games
+    env = RlGamesVecEnvWrapper(env, rl_device, clip_obs, clip_actions, obs_groups, concate_obs_groups)
+
+    # register the environment to rl-games registry
+    # note: in agents configuration: environment name must be "rlgpu"
+    vecenv.register(
+        "IsaacRlgWrapper", lambda config_name, num_actors, **kwargs: RlGamesGpuEnv(config_name, num_actors, **kwargs)
+    )
+    env_configurations.register("rlgpu", {"vecenv_type": "IsaacRlgWrapper", "env_creator": lambda **kwargs: env})
+
+    # load previously trained model
+    agent_cfg["params"]["load_checkpoint"] = True
+    agent_cfg["params"]["load_path"] = resume_path
+    print(f"[INFO]: Loading model checkpoint from: {agent_cfg['params']['load_path']}")
+
+    # set number of actors into agent config
+    agent_cfg["params"]["config"]["num_actors"] = env.unwrapped.num_envs
+    # create runner from rl-games
+    runner = Runner()
+    runner.load(agent_cfg)
+    # obtain the agent from the runner
+    agent: BasePlayer = runner.create_player()
+    agent.restore(resume_path)
+    agent.reset()
+
+    dt = env.unwrapped.step_dt
+
+    # reset environment
+    obs = env.reset()
+    if isinstance(obs, dict):
+        obs = obs["obs"]
+    timestep = 0
+    # required: enables the flag for batched observations
+    _ = agent.get_batch_size(obs, 1)
+    # initialize RNN states if used
+    if agent.is_rnn:
+        agent.init_rnn()
+    # simulate environment
+    # note: We simplified the logic in rl-games player.py (:func:`BasePlayer.run()`) function in an
+    #   attempt to have complete control over environment stepping. However, this removes other
+    #   operations such as masking that is used for multi-agent learning by RL-Games.
+    
+    if(args_cli.dataset_save_path is not None):
+        data_states = []
+        data_actions = []
+        data_dones = []
+    
+    while simulation_app.is_running():
+        start_time = time.time()
+        print(f"[INFO] Timestep: {timestep}", end="\r")
+        # run everything in inference mode
+        with torch.inference_mode():
+            # convert obs to agent format
+            obs = agent.obs_to_torch(obs)
+            # agent stepping
+            actions = agent.get_action(obs)
+            # env stepping
+            all_obs = env.env.env.get_all_obs()
+
+            obs, _, dones, _ = env.step(actions)
+            success = env.env.env.get_success_num()
+            print("timestep", timestep, "success:", success, "reward", torch.max(env.env.env._get_rewards()))
+            # perform operations for terminated episodes
+            if len(dones) > 0:
+                # reset rnn state for terminated episodes
+                if agent.is_rnn and agent.states is not None:
+                    for s in agent.states:
+                        s[:, dones, :] = 0.0
+        if args_cli.video:
+            timestep += 1
+            # exit the play loop after recording one video
+            if timestep == args_cli.video_length:
+                break
+        if args_cli.dataset_save_path is not None:
+            if args_cli.video:
+                pass
+            else:
+                timestep += 1
+
+            # convert all_obs from dict to numpy            
+            dict_numpys = {key: v.cpu().numpy() for key, v in zip(all_obs.keys(), all_obs.values())}
+            data_states.append(dict_numpys)
+            data_actions.append(actions.cpu().numpy())
+            data_dones.append(success.cpu().numpy())
+            if(timestep >= 20):
+                # save dataset
+                dataset_save_path = os.path.join(args_cli.dataset_save_path, f"{task_name}_mass_{args_cli.mass_low}_{args_cli.mass_high}_seed_{args_cli.seed}_{time.time()}.npz")
+                os.makedirs(args_cli.dataset_save_path, exist_ok=True)
+                npz_dict = {
+                    "states":  np.array(data_states,  dtype=object),
+                    "actions": np.array(data_actions, dtype=object),
+                    "dones":   np.array(data_dones,   dtype=object),
+                }
+                np.savez_compressed(dataset_save_path, **npz_dict, allow_pickle=True)
+                print(f"\n[INFO] Dataset saved to: {dataset_save_path}")
+                break
+        # time delay for real-time evaluation
+        sleep_time = dt - (time.time() - start_time)
+        if args_cli.real_time and sleep_time > 0:
+            # time.sleep(sleep_time)
+            pass
+
+    # close the simulator
+    env.close()
+
+
+if __name__ == "__main__":
+    # run the main function
+    main()
+    # close sim app
+    simulation_app.close()
---
+++ scripts/reinforcement_learning/rl_games/readme.md
@@ -0,0 +1,5 @@
+## Evaluate you model
+```bash
+./isaaclab.sh -p ./scripts/reinforcement_learning/rl_games/play.py --task Isaac-Factory-Lighter-Direct-v0 --num_envs 256 --headless  --enable_cameras 
+
+```
